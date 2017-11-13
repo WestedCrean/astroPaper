97381ca9e7c55211f1d7eee33dbb555b453e8407
WestedCrean <wikflis@gmail.com>
2017-11-13 16:49:21 +0100

    Started building scrapy spider for pulling hi-res images from links, random apod link now is returned by a function

--- apodWallpaper.py
+++ apodWallpaper.py
@@ -1,37 +1,54 @@
 import datetime
 import random
+import scrapy
 from selenium import webdriver
 from calendar import monthrange # so I don't have to deal with leap years
 #what's fucked in this code? Well when does nasa updates its apod page?
 #timezone difference sux
 #maybe just leave today's apod, there's still plenty of images
 #generate random date
-random.seed()
-now = datetime.datetime.now()
-random_date = 000000
-may_be_current_date = False
-temp = random.randint(7, now.year % 100) #7-17
-if temp == now.year % 100 :
-    may_be_current_date = True
-print("year: %d", temp)
-random_date += temp * 10000
-if may_be_current_date == True :
-    temp = random.randint(1, now.month)
-    if temp != now.month:
+
+class apodSpider(scrapy.spider):
+    name = "apod_spider"
+    allowed_domains = ["https://apod.nasa.gov/apod"]
+    start_urls = [_random_apod_link()]
+
+    def parse(self, response):
+        #Extracting content using css selectors
+        photo_link = response.css('')
+
+    def 
+    def _random_apod_link():
+        random.seed()
+        now = datetime.datetime.now()
+        random_date = 000000
         may_be_current_date = False
-else:
-    temp = random.randint(1, 12)
-    print("month: %d", temp)
-random_date += temp * 100
-if may_be_current_date == True :
-    temp = random.randint(1, now.day)
-else:
-    month_range = monthrange(now.year, now.month)
-    temp = random.randint(month_range[0], month_range[1])
-print("day : %d", temp)
-random_date += temp
-#convert to string
-random_date = str('%06d' % random_date)
-_apod_url = "https://apod.nasa.gov/apod/ap" + random_date + ".html"
-driver = webdriver.Chrome()                                 #instance of Chrome WebDriver is created
-driver.get(_apod_url)
+        temp = random.randint(7, now.year % 100) #7-17
+        if temp == now.year % 100 :
+            may_be_current_date = True
+        #print("year: "" temp)
+        random_date += temp * 10000
+        if may_be_current_date:
+            temp = random.randint(1, now.month)
+            if temp != now.month:
+                may_be_current_date = False
+        else:
+            temp = random.randint(1, 12)
+            #print("month: " temp)
+        random_date += temp * 100
+        if may_be_current_date:
+            temp = random.randint(1, now.day)
+        else:
+            month_range = monthrange(now.year, now.month)
+            temp = random.randint(month_range[0], month_range[1])
+        #print("day : " temp)
+        random_date += temp
+        #convert to string
+        random_date = str('%06d' % random_date)
+        _apod_url = "https://apod.nasa.gov/apod/ap" + random_date + ".html"
+        return _apod_url
+
+
+
+
+    if 45 != === !== <= >= ==
