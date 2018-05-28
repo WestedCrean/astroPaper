ffe018fc3569890912dc58232f392b7244c136af
WestedCrean <wikflis@gmail.com>
2018-05-28 12:30:16 +0200

    Cleaned code, to simplify code each wallpaper is an object of class Wallpaper with methods __init__ (generates random wallpaper) and setup()

--- astroPaper/api.py
+++ astroPaper/api.py
@@ -1,59 +1,21 @@
 from __future__ import print_function
 import astroPaper
-
 import sys
 import zerorpc
-import platform
 
-import tkinter as tk
-
-
-current_system = platform.system()
-print("Current system: ", current_system)
-root = tk.Tk()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-print("Current screen's width: " + str(screen_width) + " Current screen's height: " + str(screen_height))
-
-class AstroPaperApi(object):
-    currentWallpaperpath = ""
-    def roll(self):
-        """Download a random wallpaper from apod.nasa.gov"""
-        try:
-            print("main routine - api.py")
-            validFileFound = False
-            downloadSuccess = False
-            while(not downloadSuccess):
-                while(not validFileFound):
-                    check = astroPaper.rollAWallpaper()
-                    validFileFound = True
-                url = check
-                astroPaper.downloadImage(url)
-                downloadSuccess = True
-            filename = url.split('/')[-1]
-            self.currentWallpaperpath = "/code/astroPaper/astroPaper/"+filename
-            self.hasWallpaper = True
-            return filename
-        except Exception as e:
-            print(e)
+class Wallpaper:
+    wallpaper = ""
+    def __init__(self): 
+        self.wallpaper = astroPaper.newWallpaper()
     def setup(self):
-        try:
-            """Set up the latest downloaded wallpaper """
-            current_system = platform.system()
-            print("API.PY: currentWallpaperpath: {}".format(self.currentWallpaperpath))
-            astroPaper.wallpaperSetup(current_system, self.currentWallpaperpath)
-        except AttributeError:
-            print("Download a wallpaper first")
-    def display(self, image):
-        """Show downloaded wallpaper"""
-        pass
+        astroPaper.wallpaperSetup(astroPaper.getPlatform(), self.wallpaper)
 
 def parse_port():
     return 4242
 
 def main():
     addr = 'tcp://127.0.0.1:' + str(parse_port())
-    s = zerorpc.Server(AstroPaperApi())
+    s = zerorpc.Server(Wallpaper())
     s.bind(addr)
     print('start running on {}'.format(addr))
     s.run()
--- astroPaper/astroPaper.py
+++ astroPaper/astroPaper.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # ------ NASA's Astronomy Picture of the Day ------
 # -------- Random wallpaper by Wiktor Flis --------
-
+#
 import datetime
 import random
 import platform
@@ -15,160 +15,96 @@ import subprocess
 import os
 import shutil
 import __main__
-import cv2
+#import cv2
 import numpy as np
 
-
-#TODO: make clearOldWallpapers() method clear every image except the last one
-#TODO: set Linux wallpaper, probably only gnome and unity
-#TODO: check if downloaded wallpaper is ok
-#TODO: if downloaded wallpaper is a panorama (for example) and image ratio is not right,
-#have it filled with black spaces
-#TODO: make slideshow like transformations from one wallpaper to another
-#TODO: UI and "add to favourites" button
-
-#generate random date
-
-current_system = platform.system()
-
-root = tk.Tk()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-# astropaper 2.0
-
 def getValidDate():
-    random.seed()
-    randomDate = 000000 # TODO: delet dis
-    may_be_current_date = False
-    now = datetime.datetime.now()
+    d = datetime.datetime.now()
+    year = random.randint(2004, d.year)
+    month = random.randint(1, 12)
+    day = random.randint(1, 31)
+    randomDate = datetime.date(year, month, day)
+    try:
 
-    year = random.randint(7, now.year % 100)
-    if year == now.year % 100:
-        may_be_current_date = True
-    randomDate += year * 10000
-    if(may_be_current_date):
-        month = random.randint(1,now.month)
-    else:
-        month = random.randint(1,13)
-    randomDate += month * 100
-    if(may_be_current_date):
-        day = random.randint(1, now.day)
-    else:
-        if(month == 2):
-            if(abs(year - 16) % 4 == 0):
-                day = random.randint(1, 29)
-            else:
-                day = random.randint(1, 28)
-        else:
-            if(month < 8):
-                if(not month%2):
-                    day = random.randint(1, 31)
-                else:
-                    day = random.randint(1, 30)
-            else:
-                if(month%2):
-                    day = random.randint(1, 31)
-                else:
-                    day = random.randint(1, 30)
-    randomDate += day
-    print("random date: " + str(randomDate))
-    return randomDate
+        checkDate = lambda rdate : datetime.datetime.strptime(rdate, "%y%m%d")
+        print("Valid date!")
+        checkDate(randomDate)
+        randomDate = datetime.date.strftime(randomDate, "%y%m%d")
+        return randomDate
+
+    except ValueError:
+        print("Invalid date!")
+        return getValidDate()
 
 def getLink(date):
     return "https://apod.nasa.gov/apod/ap" + str(date) + ".html"
 
-def crawlURL(url):
-    directlink = "https://apod.nasa.gov/apod/"
-    imglink = ""
+def getImgLinkFromURL(url):
+    print("Trying to crawl url: " + url)
     success = False
     while(not success):
         try:
+            print("Inside while loop")
             html_page = urllib.request.urlopen(url)#sometime this line throws urllib2.HTTPError: HTTP Error 404: Not Found
             success = True
-            print("Crawled sucessfuly")
         except urllib.error.HTTPError:
-            print("Crawl Failed, retrying ((crawlURL().exception.HTTPError))")
+            print("404 getImgLinkFromURL()")
             return -1
     html_page = str(html_page.read())
     
     imglink = re.search(r"href=[\\'\"]?([^\'\" >]+)?(.jpg|.jpeg|.png)", html_page)
     if imglink:
         imglink = imglink.group(0)[6:]
-        directlink += imglink
-    print("Image direct link: " + directlink)
-    return directlink
+        #url += imglink
+    return "https://apod.nasa.gov/apod/" + imglink
+
+def getimgLink():
+    date = getValidDate()
+    link = getLink(date)
+    imgURL = getImgLinkFromURL(link)
+    return imgURL
 
 def downloadImage(url):
     global currentRandomWallpaper
     file_name = url.split('/')[-1]
     currentRandomWallpaper = file_name
-    print("Downloading " + url + " ...")
+    print("Downloading " + file_name)
     try:
-        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-                shutil.copyfileobj(response, out_file)
-                return
+        #with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        #       shutil.copyfileobj(response, out_file)
+        #       return file_name
+        return urllib.request.urlretrieve(url, '/code/astroPaper/' + currentRandomWallpaper)
     except urllib.error.HTTPError:
-        print("Request Failed, trying again. (downloadImage().exception.HTTPError)")
+        print("Request Failed, (downloadImage().exception.HTTPError)")
     except Exception:
-        print("Request Failed, trying again. (downloadImage().exception)")
-        pass
-    return currentRandomWallpaper
-    
+        print("Request Failed, (downloadImage().exception)")
 
-def rollAWallpaper():
-    date = getValidDate()
-    link = getLink(date)
-    check = crawlURL(link)
-    return check
+def getPlatform():
+    return platform.system()
 
-def mainRoutine():
+def newWallpaper():
     validFileFound = False
     downloadSuccess = False
     while(not downloadSuccess):
         while(not validFileFound):
-            check = rollAWallpaper()
-            if(check != -1):
+            imglink = getimgLink()
+            if(imglink != -1):
                 validFileFound = True
-        url = check
-
-        wallpaper = downloadImage(url)
+        wallpaper = downloadImage(imglink)
         downloadSuccess = True
-        
+    return wallpaper
 
-#def addToFavourites():
-'''
-pseudocode:
-if there is a lot of text:
-    downlaod another image
-if image_width < screen_width
-    use opencv to flip it sideways, resolution is ok so it's just about it
-now just cut the most interesting spot on the image into screen_height*screen_width resolution and voila
-'''
-def isWallpaperPretty(currentRandomWallpaper):
-    #-------------
-    # here will go openCV code for deciding if image is "pretty" (suitable for a wallpaper)
-    #-------------
-    img = cv2.imread(currentRandomWallpaper, 1)
-    img_height, img_width = img.shape[0], img.shape[1] #not working well
-    screen_width = root.winfo_screenwidth() #tolerance
-    screen_height = root.winfo_screenheight() #tolerance
-    print("Current screen's width: " + str(screen_width))
-    print("Current screen's height: " + str(screen_height))
-    print("Image width: " + str(img_width))
-    print("Image height: " + str(img_height))
-    if(img_width < screen_width or img_height < screen_height):
-        # image resolution is smaller than target resolution
-        print("Image resolution is smaller than target resolution")
-        return False
-    if(img_height > img_width):
-        #rotate
-        print("Rotating!")
+def getPath(wallpaper):
+    #TODO make folder for photographs in another place
+    wallpaper_path = os.path.abspath(__file__)
+    wallpaper_path = re.sub(__file__, '', wallpaper_path)
+    wallpaper_path = str(wallpaper_path) + str(wallpaper)
+    print("Current random wallpaper absolute path : " + wallpaper_path)
+    return wallpaper_path
 
-        np.transpose(img)
-    return True
-
-def wallpaperSetup(current_system, wallpaper_path):
+def wallpaperSetup(current_system, wallpaper):
     print("wallpaperSetup")
+    wallpaper_path = getPath(wallpaper)
     #setting up the wallpaper, depending on a system
     if current_system == "Windows":
         #windows set up
@@ -184,11 +120,11 @@ def wallpaperSetup(current_system, wallpaper_path):
     if current_system == "Darwin":
         print("MacOS script")
         try:
-            setWallpaperScript = """/usr/bin/osascript<<END tell application "Finder" set desktop picture to POSIX file "{}" end tell END"""
-            os.system(setWallpaperScript.format(wallpaper_path))
+            setWallpaperCommand = "osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"" + wallpaper_path + "\"'"
+            os.system(setWallpaperCommand)
             print("Wallpaper set up!")
         except:
-            print("osascript not working")
+            print("Appscript not installed.")
         #macos set up
 
     if current_system == "Linux":
@@ -199,32 +135,13 @@ def wallpaperSetup(current_system, wallpaper_path):
         except:
             print("gsettings not working")
 
-def getPath(wallpaper):
-    #TODO make folder for photographs in another place
-    wallpaper_path = os.path.abspath(__file__)
-    print("Wallpaper path: " + str(wallpaper_path))
-    wallpaper_path = re.sub(r'astroPaper.py', '', wallpaper_path)
-    wallpaper_path = str(wallpaper_path) + str(wallpaper)
-    print("Current random wallpaper absolute path : " + wallpaper_path)
-    return wallpaper_path
-
-def clearOldWallpapers(dir, lastWallpaperName): #add global wallpaper save folder string
-    for file in os.listdir(dir):
-        if(file != "apodWallpaper.py" or file != maxfile):
-            if file.endswith('.jpg' or '.png'):
-                os.remove(os.path.join(dir, file))
-'''
 def main():
-
-    path = "/Users/WestedCrean/Pictures/"
-    wallpaper = ""
-    mainRoutine()
-    
-    wallpaperSetup(current_system, currentRandomWallpaper)
+    current_system = getPlatform()
+    wallpaper = newWallpaper()
+    wallpaperSetup(current_system, wallpaper)
     print("Done")
-
+    
 
 
 if __name__ == "__main__":
     main()
-'''
