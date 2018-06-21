de33a1a67fb0d142e6c5a37f0a164a5b43898f70
WestedCrean <wikflis@gmail.com>
2018-06-21 12:03:07 +0200

    changed a bit prototype design

--- Client/astropaper_client.js
+++ Client/astropaper_client.js
@@ -3,35 +3,47 @@ var services = require('./apservice_grpc_pb');
 var EventEmitter = require('events').EventEmitter;
 var grpc = require('grpc');
 
+
+var getWallpaper = function (client, request, q) {
+  let e = new EventEmitter();
+  process.nextTick(function(){
+    request.setQuantity(q);
+    e.emit('start');
+    client.getNewWallpaper(request, function(err, response) {
+      var wallpaper = response.array[0]
+      e.emit('end')
+      console.log('Wallpaper:', wallpaper)
+      });
+  });
+  return(e);
+}
+
 function main() {
   console.log("Client is running");
-  var client = new services.AstropaperClient('localhost:50050',
-                                       grpc.credentials.createInsecure());
+  var client = new services.AstropaperClient('localhost:50050', grpc.credentials.createInsecure());
   var request = new messages.WallpaperRequest();
-  
-  var quantity;
-  if (process.argv.length >= 3) {
-    quantity = process.argv[2];
-  } else {
-    quantity = 1;
-  }
 
-  request.setQuantity(quantity);
-  client.getNewWallpaper(request, function(err, response) {
-    console.log('Wallpaper:', response.array[0]);
-    var wallpaper = response.array[0];
+  var w = getWallpaper(client, request, 1);
+  w.on('start',() => {
+    console.log('calling wallpaper')
+    // render download bar
+  });
+  w.on('end', () => {
+    console.log('downloaded')
+    // destroy download bar
+    var setup = new messages.SetupRequest();
+    setup.setWallpaper(w);
+    client.setupWallpaper(setup, function(err, response) {
+      console.log('', w);
+    });
+  });
+
     // chained get + setup
     // TODO: make it event driven
     // 
     // ex: 6 calls for new wallpapers
     // on each done => display in app
     // on event 'checked' and 'setup' call setup
-    var setup = new messages.SetupRequest();
-    setup.setWallpaper(wallpaper);
-    client.setupWallpaper(setup, function(err, response) {
-      console.log('', wallpaper);
-    });
-  });
 }
 
 main();
